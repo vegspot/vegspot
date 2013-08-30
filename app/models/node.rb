a307ea3cc5354674da7ea6c1fb2dba1a63057195
Mike Bajur <mbajur@gmail.com>
2013-08-30 17:54:37 +0200

    Auto assign site to node

--- app/models/node.rb
+++ app/models/node.rb
@@ -2,8 +2,24 @@ class Node < ActiveRecord::Base
   belongs_to :user
   belongs_to :site
 
+  before_save :set_site
+
   mount_uploader :thumbnail, ThumbnailUploader
 
   scope :popular, -> { order('created_at ASC') }
   scope :recent,  -> { order('created_at DESC') }
+
+
+  private
+
+  # Check if site exists in database. If not, create it
+  # and assign it to the node. If not, find and assign.
+  def set_site
+    uri = URI.parse(self.url)
+    uri = URI.parse("http://#{url}") if uri.scheme.nil?
+    host = uri.host.downcase
+    host.start_with?('www.') ? host[4..-1] : host
+
+    self.site = Site.find_or_create_by(address: host)
+  end
 end
