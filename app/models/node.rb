27fdc1e2eee46c3f4cce1661fe7f020836938bc1
Mike Bajur <mbajur@gmail.com>
2013-08-30 18:49:38 +0200

    Add proper callback for fetch_thumbnail

--- app/models/node.rb
+++ app/models/node.rb
@@ -7,17 +7,12 @@ class Node < ActiveRecord::Base
   mount_uploader :thumbnail, ThumbnailUploader
 
   # callbacks
-  before_save :set_site
+  before_save  :set_site
+  after_create :fetch_thumbnail
 
   # scopes
   scope :popular, -> { order('created_at ASC') }
   scope :recent,  -> { order('created_at DESC') }
-
-  def fetch_thumbnail
-    doc = Nokogiri::HTML(open(self.url))
-    image = doc.css('img')
-    throw image.first[:src]
-  end
 
 
   private
@@ -31,5 +26,14 @@ class Node < ActiveRecord::Base
     host.start_with?('www.') ? host[4..-1] : host
 
     self.site = Site.find_or_create_by(address: host)
+  end
+
+  # Fetch URL and look for first image from there.
+  # Than, set it as node thumbnail.
+  def fetch_thumbnail
+    doc = Nokogiri::HTML(open(self.url))
+    image = doc.css('img')
+    self.remote_thumbnail_url = image.first[:src]
+    self.save!
   end
 end
