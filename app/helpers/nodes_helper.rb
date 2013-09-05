48c4cd19e5360822d1e2c238bdefb00521c37040
Mike Bajur <mbajur@gmail.com>
2013-09-05 11:27:33 +0200

    Make frot page working for signed out users

--- app/helpers/nodes_helper.rb
+++ app/helpers/nodes_helper.rb
@@ -1,2 +1,15 @@
 module NodesHelper
+
+  # Determine if widget-score should have
+  # voted-for or voted-against class
+  def voted_widget_class(node)
+    return if !user_signed_in?
+
+    if current_user.voted_for?(node)
+      "voted-for"
+    elsif current_user.voted_against?(node)
+      "voted-against"
+    end
+  end
+
 end
