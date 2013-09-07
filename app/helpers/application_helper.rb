0e12abf3d17cc4f17998a8919810071860a93968
Mike Bajur <mbajur@gmail.com>
2013-09-07 10:27:56 +0200

    Remove unnecessary code from markdown helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,6 @@ module ApplicationHelper
   end
 
   def markdown(text)
-    options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, hard_wrap: true, filter_html: true, autolink: true, no_intraemphasis: true, fenced_code: true, gh_blockcode: true)
     markdown.render(text).html_safe
   end
