4dd2cda2cfe2ca7eb7e6e57eb8177fc2d0b2accd
Mike Bajur <mbajur@gmail.com>
2013-09-27 21:25:53 +0200

    Code cleaning

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,13 +1,17 @@
 module TagsHelper
 
-  def tags_as_labels(node)
-    return_html = ''
+  # Generate list of tags formatted as
+  # downcased links with hashes before their names
+  def tag_list_formatted(node)
+    return_html = '<ul class="list-inline tags">'
 
     node.tags.each do |tag|
-      return_html << link_to(tag.name, '#', class: 'label label-tag')
+      return_html << content_tag(:li, link_to("##{tag.name.downcase}", '#'))
     end
 
-    return return_html.html_safe
+    return_html << '</ul>'
+
+    return return_html.html_safe  	
   end
 
 end
