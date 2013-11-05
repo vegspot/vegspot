0152ad86a753212cf81e2b9a39140ee56efc8607
Mike Bajur <mbajur@gmail.com>
2013-11-05 20:09:10 +0100

    Add image size for avatar

--- clock.rb
+++ clock.rb
@@ -3,7 +3,7 @@ include Clockwork
 require './config/boot'
 require './config/environment'
 
-every(6.hour, 'node.refresh_score', at: '**:00') do
+every(3.hour, 'node.refresh_score', at: '**:00') do
   Node.live.this_month.each do |node|
     NodesWorker.perform_async(:refresh_score, node.id)
   end
